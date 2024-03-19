d169e12a58e917eb27d8f3939ce9c0516bc0791d
nalisherovvna <mushknasiba@gmail.com>
2024-03-19 19:42:45 +0500

    it was my homework

---
+++ main.js
@@ -0,0 +1,48 @@
+// // const front951 = ["Zubayr","Madina" ,"Muhammadjon"];
+// // let name1 = front951[2];
+
+// // alert(name1);
+
+
+// const front951 = [];
+// front951[0] = "Xusniya"
+// front951[1] = "Madina"
+// front951[2] = "Zubayr"
+// front951[3] = "Nasiba"
+
+// alert(front951);
+
+
+// console.log  =  ( "Olma Banan Gilos Shaftoli ");
+// let uzunli = ["Boshlanishida arrayingizni uzunligi 4"];
+// confirm= ("Siz arrayingizdan ma'lumotlarni olib tashlamohchimisz?");
+// let count = long.length;
+
+// // let element = long.pop();
+
+// // alert(element + "\n" + long);
+
+// alert(uzunli);
+// alert(count);
+
+
+const array = ["Mars", "Yupiter", "Saturin", "Venera"];
+let long = array.length;
+console.log("Arrayning uzunligi  = " + long);
+alert("Arrayning uzunligi  = " + long);
+
+
+let result = confirm("Siz jarayon davom etishini hohlaysizmi?");
+
+
+if (result) {
+    array.pop();
+    let1 = array.length;
+    alert("Kesilgan arrayning uzunligi  = " + 1);
+    console.log("Kesilgan arrayning uzunligi  = " + 1);
+} else {
+    let1 = array.length;
+    alert("Siz jarayyoni toxtatishingiz uchun  =" + 1);
+    console.log("Siz jarayonni toxtashingiz uchun  =" + 1);
+}
+
